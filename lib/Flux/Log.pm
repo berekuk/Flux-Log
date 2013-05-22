5b24bdec95367265a37f32bb8b0d42c2424a46be
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-05-22 21:44:34 +0400

    ClientList role; tests are passing

--- lib/Flux/Log.pm
+++ lib/Flux/Log.pm
@@ -12,17 +12,14 @@ It supports safe writes to rotating logs, i.e. it will automatically switch to t
 
 use Moo;
 extends 'Flux::File';
-
-#with 'Flux::Storage::Role::ClientList';
+with 'Flux::Storage::Role::ClientList';
 
 use Type::Params qw(validate);
-use Types::Standard qw(HashRef);
+use Types::Standard qw(Str Dict HashRef);
 
-use Digest::MD5 qw(md5_hex);
+use File::Basename qw(basename);
 
 use Flux::Log::In;
-use Scalar::Util qw(blessed reftype);
-use Carp;
 
 has '+reopen' => (
     default => sub { 1 },
@@ -33,21 +30,55 @@ sub description {
     return "log: ".$self->file;
 }
 
+has 'client_dir' => (
+    is => 'lazy',
+    isa => Str,
+    default => sub {
+        my $self = shift;
+        my $dir = $self->file.".pos";
+        unless (-d $dir) {
+            mkdir $dir or die "mkdir failed: $!";
+        }
+        return $dir;
+    },
+);
+
 =head1 METHODS
 
 =over
 
-=item B<in($unrotate_params)>
+=item B<in($client_name)>
 
-Construct input stream from a client name or an L<Flux::Log::Cursor> object.
+=item B<in({ pos => $posfile })>
 
-C<$unrotate_params> can contain more unrotate options.
+Construct input stream from a client name or a posfile name.
 
 =cut
 sub in {
     my $self = shift;
-    my ($unrotate_params) = validate(\@_, HashRef);
-    return Flux::Log::In->new({ log => $self->file, unrotate => $unrotate_params });
+    my ($client_or_params) = validate(\@_, Str | Dict[pos => Str]);
+
+    if (ref $client_or_params) {
+        return Flux::Log::In->new({
+            log => $self->file,
+            unrotate => $client_or_params,
+        });
+
+    }
+    else {
+        return Flux::Log::In->new({
+            log => $self->file,
+            unrotate => {
+                pos => $self->client_dir."/".$client_or_params
+            }
+        });
+    }
+}
+
+sub client_names {
+    my $self = shift;
+    my @files = glob $self->client_dir.'/*';
+    return map { basename $_ } @files;
 }
 
 =back
